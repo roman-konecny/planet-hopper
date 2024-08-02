0eda88ff096b56ecf9c7db9022a3f2dbece36ea7
Roman Konecny <roman.konecny41@gmail.com>
2024-08-02 13:10:49 +0200

    sprite and enTities system

--- entry_planet-hopper.c
+++ entry_planet-hopper.c
@@ -1,48 +1,132 @@
+#define MAX_ENTITY_COUNT 1024
+
+typedef struct Sprite {
+	Gfx_Image* image;
+	Vector2 size;
+} Sprite;
+
+typedef enum SpriteID {
+	SPRITE_nil,
+	SPRITE_player,
+	SPRITE_planet,
+	SPRITE_antena,
+	SPRITE_MAX,
+} SpriteID;
+
+Sprite sprites[SPRITE_MAX];
+Sprite* get_sprite(SpriteID id) {
+	if (id >= 0 && id < SPRITE_MAX) {
+		return &sprites[id];
+	}
+	return &sprites[0];
+}
 
 typedef enum EntityArchetype {
-	nil = 0,
-	planet = 1,
-	player = 2,
-	shoes = 3,
-	antena = 4	
-}
+	arch_nil = 0,
+	arch_planet = 1,
+	arch_player = 2,
+	arch_shoes = 3,
+	arch_antena = 4	
+} EntityArchetype;
 
 typedef struct Entity {
 	bool is_valid;
 	EntityArchetype arch;
 	Vector2 pos;
-
+	bool render_sprite;
+	SpriteID sprite_id;
 } Entity;
 
+typedef struct World {
+	Entity entities[MAX_ENTITY_COUNT];
+} World;
+
+World* world = 0;
+
+Entity* entity_create() {
+	Entity* entity_found = 0;
+	for (int i = 0; i < MAX_ENTITY_COUNT; i++) {
+		Entity* existing_entity = &world->entities[i];
+		if (!existing_entity->is_valid) {
+			entity_found = existing_entity;
+			break;
+		}
+	}
+	assert(entity_found, "No more free entities");
+	entity_found->is_valid = true;
+	return entity_found;
+}
+
+void entity_destroy(Entity* entity) {
+	memset(entity, 0, sizeof(Entity));
+}
+
+void setup_player(Entity* en) {
+	en->arch = arch_player;
+	en->sprite_id = SPRITE_player;
+}
+
+void setup_planet(Entity* en) {
+	en->arch = arch_planet;
+	en->sprite_id = SPRITE_planet;
+}
 
 int entry(int argc, char **argv) {
 	
+	// Window Setup
 	window.title = STR("planet-hopper");
 	window.scaled_width = 1280; // We need to set the scaled size if we want to handle system scaling (DPI)
 	window.scaled_height = 720; 
 	window.x = 200;
-	window.y = 90;
+	window.y = 200;
 	window.clear_color = hex_to_rgba(0x000000ff);
 
-	Gfx_Image* player = load_image_from_disk(fixed_string("pics/player.png"), get_heap_allocator());
-	Gfx_Image* planet1 = load_image_from_disk(fixed_string("pics/planet1.png"), get_heap_allocator());
-	assert(player, "fuckkk");
+	// Game Objects Creation
+	world = alloc(get_heap_allocator(), sizeof(World));
+	memset(world, 0, sizeof(World));
 
+	sprites[SPRITE_planet] = (Sprite){ .image=load_image_from_disk(STR("pics/planet1.png"), get_heap_allocator()), .size=v2(250.0, 250.0) };
+	sprites[SPRITE_player] = (Sprite){ .image=load_image_from_disk(STR("pics/player.png"), get_heap_allocator()), .size=v2(70.0, 70.0) };
+
+	Entity* planet1_en = entity_create();
+	setup_planet(planet1_en);
+	Entity* player_en = entity_create();
+	setup_player(player_en);
+
+	// Game Loop
 	float64 last_time = os_get_current_time_in_seconds();
-
-	Vector2 player_pos = v2(0,0);
-
-
 	while (!window.should_close) {
+
+		// Setups + FPS
 		reset_temporary_storage();
 		draw_frame.projection = m4_make_orthographic_projection(window.width * -0.5, window.width * 0.5, window.height * -0.5, window.height * 0.5, -1, 10);
-
 		float64 now = os_get_current_time_in_seconds();
 		if ((int)now != (int)last_time) log("%.2f FPS\n%.2fms", 1.0/(now-last_time), (now-last_time)*1000);
 		float64 delta_t = now - last_time;
 		last_time = now;
-		
 
+		// :render
+		for (int i = 0; i < MAX_ENTITY_COUNT; i++) {
+			Entity* en = &world->entities[i];
+			if (en->is_valid) {
+
+				switch (en->arch) {
+
+					default:
+					{
+						Sprite* sprite = get_sprite(en->sprite_id);
+						Matrix4 xform = m4_scalar(1.0);
+						xform         = m4_translate(xform, v3(en->pos.x, en->pos.y, 0));
+						xform         = m4_translate(xform, v3(sprite->size.x * -0.5, 0.0, 0));
+						draw_image_xform(sprite->image, xform, sprite->size, COLOR_WHITE);
+						break;
+					}
+				}
+
+			}
+		}
+		
+		// Game controll logic
 		if (is_key_just_pressed(KEY_ESCAPE)) {
 			window.should_close = true;
 		}
@@ -62,18 +146,11 @@ int entry(int argc, char **argv) {
 		}
 		input_axis = v2_normalize(input_axis);
 
-		int speed = 75
+		// Player movement
+		int speed = 175;
+		player_en->pos = v2_add(player_en->pos, v2_mulf(input_axis, speed * delta_t));
 
-		player_pos = v2_add(player_pos, v2_mulf(input_axis, speed * delta_t));
-
-		Matrix4 xform_player = m4_scalar(1);
-		Matrix4 xform_planet1 = m4_scalar(1);
-		xform_planet1         = m4_translate(xform_planet1, v3(-0.5, -0.5, 0));
-		draw_image_xform(planet1, xform_planet1, v2(250, 250), COLOR_WHITE);
-		
-		xform_player         = m4_translate(xform_player, v3(player_pos.x, player_pos.y, 0));
-		draw_image_xform(player, xform_player, v2(32, 37), COLOR_WHITE);
-		
+		// Updates		
 		os_update(); 
 		gfx_update();
 	}
